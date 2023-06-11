f960ee48e41ee3e794f36e675657ae5bd98b8f81
maanong <58322744+maanong@users.noreply.github.com>
2023-06-11 23:36:31 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -37,27 +37,27 @@ async def on_message(message):
     if message.content.startswith(f'{PREFIX}앨범'):
         await message.channel.send('https://www.youtube.com/watch?v=cKmhab15cCc')
     if message.content.startswith(f'{PREFIX}?'):
-    keyword = message.content.replace('', f'{PREFIX}?')
-    url = f'https://www.youtube.com/results?search_query={keyword}'
+        keyword = message.content.replace('', f'{PREFIX}?')
+        url = f'https://www.youtube.com/results?search_query={keyword}'
 
-    msg = await message.channel.send(embed=discord.Embed(title="잠시만 기다려주세요!\n정보를 수집 중이므로 다소 시간이 걸릴 수 있습니다.",
+        msg = await message.channel.send(embed=discord.Embed(title="잠시만 기다려주세요!\n정보를 수집 중이므로 다소 시간이 걸릴 수 있습니다.",
                                                          description=f"[ {message.author.mention} ]", color=0xFF9900))
 
-    options = Options()
-    options.headless = True
-    driver = webdriver.Chrome(executable_path="./chromedriver.exe", options=options)
-    driver.get(url)
+        options = Options()
+        options.headless = True
+        driver = webdriver.Chrome(executable_path="./chromedriver.exe", options=options)
+        driver.get(url)
 
-    soup = BeautifulSoup(driver.page_source, "html.parser")
-    driver.close()
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+        driver.close()
 
-    video_info = soup.find("a", attrs={"id": "video-title"})
-    title = video_info.get("title")
-    visit = video_info.get("aria-label").split(" ")[-1]
-    href = video_info.get("href")
+        video_info = soup.find("a", attrs={"id": "video-title"})
+        title = video_info.get("title")
+        visit = video_info.get("aria-label").split(" ")[-1]
+        href = video_info.get("href")
 
-    await msg.delete()
-    await message.channel.send(f"**{keyword} 의 검색 결과입니다.**\n\n{title} | 조회수 {visit}\nhttp://youtube.com{href}")
+        await msg.delete()
+        await message.channel.send(f"**{keyword} 의 검색 결과입니다.**\n\n{title} | 조회수 {visit}\nhttp://youtube.com{href}")
 
 
 try:
