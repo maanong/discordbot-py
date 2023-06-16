f3c8e0f36b01416564cc9d62aba77a3bf1a59f24
maanong <58322744+maanong@users.noreply.github.com>
2023-06-16 15:02:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -33,6 +33,17 @@ async def on_message(message):
         
     if message.content.startswith(f'{PREFIX}앨범'):
         await message.channel.send('https://www.youtube.com/watch?v=cKmhab15cCc')
+@bot.command
+async def game(ctx, user: str):  # user:str로 !game 다음에 나오는 메시지를 받아줌
+    rps_table = ['가위', '바위', '보']
+    bot = random.choice(rps_table)
+    result = rps_table.index(user) - rps_table.index(bot)  # 인덱스 비교로 결과 결정
+    if result == 0:
+        await ctx.send(f'{user} vs {bot}  비겼습니다.')
+    elif result == 1 or result == -2:
+        await ctx.send(f'{user} vs {bot}  유저가 이겼습니다.')
+    else:
+        await ctx.send(f'{user} vs {bot}  봇이 이겼습니다.')
 try:
     client.run(TOKEN)
 except discord.errors.LoginFailure as e:
